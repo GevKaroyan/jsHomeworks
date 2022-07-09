cbb9e6a096264a19653c507fad2b965abbc985ad
GevKaroyan <karoyangevorg87@gmail.com>
2022-07-09 20:23:25 +0300

    my js homeworks

---
+++ js/lesson-20.js
@@ -0,0 +1,47 @@
+"use strict";
+
+//1․ ստեղծում եք ցանկացած թեմատիկային փոքր կոնսոլային հավելված, որը չպետք ա կախում ունենա բրաուզերից
+
+//2․ ձեր կոնսոլային հավելվածի մեջ օգտագործում եք հետևյալ մեթոդները՝ toUpperCase, toLowerCase, slice, trim, split, join, forEach
+
+//3. աշխատացնում եք console-ում 
+
+//Հ․Գ․ ցանկալի ա կոնսոլում ֆունկցիա գրվի, արգումենտ փոխանցվի, ու ըստ էդ արգումենտի կատարվեն ներքին մեխանիզմները :)
+
+// const myCity = {
+//     cityNames: [
+//         { name1: "Վաղարշապատ" },
+//         { name2: "Էջմիածին" },
+//         { name3: "Վարդգեսավան"},
+//         { name4: "Արտիմեդ" }
+//     ],
+//     churchs: [
+//         {church1: "Մայր աթոռ սուրբ Էջմիածին"},
+//         {church2: "Սուրբ Հռիփսիմե եկեղեցի"},
+//         {church3: "Սուրբ Գայանե եկեղեցի"},
+//         {church4: "Սուրբ Շողակաթ եկեղեցի"},
+//         {church5: "Սուրբ Մարիամ Աստվածածին"}
+//     ],    
+// };
+// console.log(myCity.cityNames);
+
+// let str = "Վաղարշապատ (առավել հայտնի է Էջմիածին անվանումով),Հայաստանի քաղաքներից մեծությամբ չորրորդն է։";
+// let newStrSlice = str.slice(0, 10);
+// console.log(newStrSlice);
+// let newStrUpper = str.toUpperCase();
+// console.log(newStrUpper);
+// let newStrLower = str.toLowerCase();
+// console.log(newStrLower);
+// let newStrTRim = str.trim();
+// console.log(newStrTRim );
+// let newStrSplit = str.split();
+// console.log(newStrSplit);
+// let arr = newStrSplit.join();
+// console.log(arr);
+
+// const array = [5, 6, 8, 7, 3, 10];
+// array.forEach(function (item, index) {
+    
+//     console.log(`${index}: ${item}`);
+// });
+
